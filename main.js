0345bbc6b8b7c6ebf2eef7018b7be54b739d2801
blha303 <stevensmith.ome@gmail.com>
2016-10-06 03:07:11 +0800

    Moved styles and js out of index.html

---
+++ main.js
@@ -0,0 +1,60 @@
+var root = window.location.protocol + "//" + window.location.hostname + "/";
+var input_history = [];
+var input_history_curpos = 0;
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4) {
+            if (xmlHttp.status == 200) callback(xmlHttp.responseText);
+            else callback("Command not recognized.");
+        }
+    }
+    xmlHttp.open("GET", theUrl, true);
+    xmlHttp.send(null);
+}
+
+function add_text_block(text) {
+       	elem = document.createElement("div");
+       	elem.className = "textblock";
+       	elem.innerHTML = text;
+       	document.getElementsByTagName("form")[0].insertBefore(elem,document.getElementById("inputblock"));
+       	document.getElementById("inputblock").scrollIntoView();
+}
+
+function submit_handler() {
+       	form = document.getElementsByTagName("form")[0];
+       	input_history.push(document.getElementById("textentry").value);
+       	input_history_curpos = input_history.length-1;
+       	document.getElementById("textentry").value = "";
+       	// Command feedback
+       	e1 = document.createElement("div");
+       	e1.className = "textblock";
+       	t1 = document.createTextNode("> " + input_history[input_history_curpos]);
+       	e1.appendChild(t1);
+       	form.insertBefore(e1,document.getElementById("inputblock"));
+       	document.getElementById("inputblock").scrollIntoView();
+       	// Command output
+       	httpGetAsync(root + input_history[input_history_curpos] + ".txt", add_text_block);
+}
+
+window.onload = function() {
+       	httpGetAsync(root + "location.txt", function(resp){
+       		document.getElementById("top").innerHTML = resp;
+       	});
+       	httpGetAsync(root + "l.txt", add_text_block);
+};
+document.onkeydown = function(e) {
+       	e = e || window.event;
+       	input = document.getElementById("textentry");
+       	if (e.keyCode == "38" && document.activeElement === input && input_history_curpos > 0) {
+       		input_history_curpos -= 1;
+       		input.value = input_history[input_history_curpos];
+       		setTimeout(function(){ input.selectionStart = input.selectionEnd = 10000; }, 0);
+       	}
+       	if (e.keyCode == "40" && document.activeElement === input && input_history_curpos < input_history.length-1) {
+       		input_history_curpos += 1;
+       		input.value = input_history[input_history_curpos];
+       		setTimeout(function(){ input.selectionStart = input.selectionEnd = 10000; }, 0);
+       	}
+}
